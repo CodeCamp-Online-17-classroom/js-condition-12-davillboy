313054ccdd91d7f170e1c52c1ccd5ffa241ef56e
davillboy <71083807+davillboy@users.noreply.github.com>
2024-04-22 09:51:34 +0700

    first change

--- app.js
+++ app.js
@@ -1,1 +1,32 @@
 // write code here
+<script>
+        function calculateTicketPrice(zone) {
+            let ticketPrice;
+          
+            switch (zone.toUpperCase()) {
+              case "A":
+                ticketPrice = 1000;
+                break;
+              case "B":
+                ticketPrice = 800;
+                break;
+              case "C":
+                ticketPrice = 500;
+                break;
+              default:
+                ticketPrice = "ไม่พบ Zone"; 
+            }
+          
+            return ticketPrice;
+          }
+          
+          let zoneInput = prompt("Enter ticket zone (A, B, or C):").toUpperCase();
+          let price = calculateTicketPrice(zoneInput);
+          
+          if (typeof price === "number") {
+            alert("Ticket Price: " + price);
+          } else {
+            alert(price);
+          }
+          
+    </script>
